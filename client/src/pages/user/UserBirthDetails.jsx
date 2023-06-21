886dc6b860618d3c33332bc3ad998c0da4e4cdd1
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-06-21 17:47:13 +0530

    added modal to userBIrthDaetails

--- client/src/pages/user/UserBirthDetails.jsx
+++ client/src/pages/user/UserBirthDetails.jsx
@@ -1,10 +1,11 @@
 import BirthDetailsImage from "../../assets/birthDetailsBanner.png";
+
 const UserBirthDetails = () => {
   return (
     <>
       <div>
         <div className="p-4">
-          <header className="w-full  h-auto bg-[#EBF3FE] rounded-lg flex justify-between relative">
+          <header className="w-full h-auto bg-[#EBF3FE] rounded-lg flex justify-between relative">
             <div className="px-8 py-16">
               <h4 className="font-semibold text-2xl">Birth Details</h4>
               <p className="text-gray-500 font-light">Enter Birth Details</p>
@@ -18,144 +19,153 @@ const UserBirthDetails = () => {
             </div>
           </header>
 
-          {/* create table  */}
+          {/* create table */}
           <div className="flex justify-end mt-4">
             <button
               onClick={() => window.addBirthDetailsModal.showModal()}
-              className="btn btn-secondary"
+              className="btn btn-info text-white"
             >
               Add Birth Details
             </button>
           </div>
-
-          <div className="mt-8">
-            <h4 className="text-xl">Birth Details</h4>
-            <form action="">
-              <div className="w-full flex flex-col  sm:flex-row gap-4">
-                <div className="form-control w-full">
-                  <label htmlFor="firstName" className="label">
-                    <span className="label-text">First Name</span>
-                  </label>
-                  {/* {console.log(userProfile)} */}
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("firstName")}
-                    type="text"
-                    placeholder="Name of Person"
-                    // defaultValue={userProfile?.firstName}
-                    name="firstName"
-                    id="firstName"
-                  />
-                </div>
-                <div className="form-control w-full">
-                  <label htmlFor="lastName" className="label">
-                    <span className="label-text">Last Name</span>
-                  </label>
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    placeholder="Last Name"
-                    // {...register("lastName")}
-                    type="text"
-                    // defaultValue={userProfile?.lastName}
-                    name="lastName"
-                    id="lastName"
-                  />
-                </div>
-              </div>
-
-              <div className="w-full flex flex-col  sm:flex-row gap-4">
-                <div className="form-control w-full">
-                  <label htmlFor="firstName" className="label">
-                    <span className="label-text">Gender</span>
-                  </label>
-                  {/* {console.log(userProfile)} */}
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("firstName")}
-                    placeholder="Gender"
-                    type="text"
-                    // defaultValue={userProfile?.firstName}
-                    name="Gender"
-                    id="Gender"
-                  />
-                </div>
-                <div className="form-control w-full">
-                  <label htmlFor="firstName" className="label">
-                    <span className="label-text">Date of Birth</span>
-                  </label>
-                  {/* {console.log(userProfile)} */}
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("firstName")}
-                    placeholder="DD/MM/YYYY"
-                    type="text"
-                    // defaultValue={userProfile?.firstName}
-                    name="DateofBirth"
-                    id="DateofBirth"
-                  />
-                </div>
-              </div>
-
-              <div className="w-full flex flex-col  sm:flex-row gap-4">
-                <div className="form-control w-full">
-                  <label htmlFor="firstName" className="label">
-                    <span className="label-text">Place of Birth</span>
-                  </label>
-                  {/* {console.log(userProfile)} */}
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("firstName")}
-                    placeholder="type few characters and wait for the drop-down"
-                    type="text"
-                    // defaultValue={userProfile?.firstName}
-                    name="PlaceofBirth"
-                    id="PlaceofBirth"
-                  />
-                </div>
-                <div className="form-control w-full">
-                  <label htmlFor="lastName" className="label">
-                    <span className="label-text">Birth Time</span>
-                  </label>
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("lastName")}
-                    type="text"
-                    placeholder="Time of Birth"
-                    // defaultValue={userProfile?.lastName}
-                    name="BirthTime"
-                    id="BirthTime"
-                  />
-                </div>
-              </div>
-              <div className="w-full flex flex-col  sm:flex-row gap-4">
-                <div className="form-control w-full">
-                  <label htmlFor="firstName" className="label">
-                    <span className="label-text">Message</span>
-                  </label>
-                  {/* {console.log(userProfile)} */}
-                  <input
-                    className="input input-sm border  border-3 border-gray-400"
-                    // {...register("firstName")}
-                    placeholder="Type things about yourself minnimum 30 words"
-                    type="text"
-                    // defaultValue={userProfile?.firstName}
-                    name="Message"
-                    id="Message"
-                  />
-                </div>
-              </div>
-              <div className="flex gap-4 mt-4 justify-start">
-                <button type="submit" className="btn btn-info">
-                  Submit Your Details
-                </button>
-              </div>
-            </form>
-          </div>
         </div>
         <dialog id="addBirthDetailsModal" className="modal">
-          <form method="dialog" className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click outside to close</p>
+          <form method="dialog w-[40vw]" className="modal-box">
+            <h2 className="py-4 text-2xl">Enter Birth Details</h2>
+            <hr />
+            {/* first row */}
+            <div className="flex gap-4">
+              {/* first name */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">First Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+              {/* last name */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+            </div>
+            {/* second row */}
+            <div className="flex gap-4">
+              {/* gender */}
+              <div className="form-control w-full max-w-xs">
+                <div className="mt-4">
+                  <label
+                    htmlFor="gender"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Gender
+                  </label>
+                  <div className="mt-1">
+                    <select
+                      id="gender"
+                      name="gender"
+                      className="input input-bordered w-full max-w-xs"
+                    >
+                      <option>Male</option>
+                      <option>Female</option>
+                      <option>Other</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              {/* last name */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Last Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+            </div>
+            {/* third row */}
+            <div className="flex gap-4">
+              {/* Date of Birth */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Date of Birth</span>
+                </label>
+                <input
+                  type="date"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+              {/* Place of birth*/}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Place of Birth</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+            </div>
+            {/* fourth row */}
+            <div className="flex gap-4">
+              {/* birth time */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Birth Time</span>
+                </label>
+                <input
+                  type="time"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+              {/* message */}
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">Message</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="max 30 characters"
+                  className="input input-bordered w-full max-w-xs"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="mt-4">
+                <label
+                  htmlFor="gender"
+                  className="block  text-sm font-medium text-gray-700"
+                >
+                  Type
+                </label>
+                <div className="mt-1">
+                  <select
+                    id="gender"
+                    name="gender"
+                    className="input input-bordered w-full max-w-xs"
+                  >
+                    <option>Self</option>
+                    <option>Family Member</option>
+                    <option>Other</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <button className="btn btn-primary w-full mt-4">Add</button>
           </form>
           <form method="dialog" className="modal-backdrop">
             <button>close</button>
