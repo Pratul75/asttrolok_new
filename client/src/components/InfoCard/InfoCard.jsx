4f57ae6136927cbd9b2679547092432524513a1f
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-06-22 19:14:51 +0530

    added changes

--- client/src/components/InfoCard/InfoCard.jsx
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const InfoCard = ({ heading, subHeading, time, icon, color }) => {
   return (
-    <div  className="flex border-4  justify-between items-center my-4 hover:bg-gray-100 px-4 py-2 rounded-lg ">
+    <div className="flex justify-between cursor-pointer items-center my-4 hover:bg-gray-100 px-4 py-2 rounded-lg ">
       <div className="flex gap-4">
         {/* avatar */}
         <div className="avatar placeholder">
@@ -12,7 +12,7 @@ const InfoCard = ({ heading, subHeading, time, icon, color }) => {
         </div>
         {/* info  */}
         <div>
-          <h4 className="text-[16px] cursor-pointer  font-semibold">{heading}</h4>
+          <h4 className="text-[16px]   font-semibold">{heading}</h4>
           <p>{subHeading}</p>
         </div>
       </div>
