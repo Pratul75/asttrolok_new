0c794be0e12f9def9e368de9433bc8f7ebd4ed58
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-06-24 13:11:12 +0530

    added changes

--- client/src/components/CoursesListCard/index.jsx
+++ client/src/components/CoursesListCard/index.jsx
@@ -1,10 +1,67 @@
-import CourseListImage1 from "../../assets/CourseListImage1.png";
-const CourseListCard = () => {
+import { BsThreeDotsVertical } from "react-icons/bs";
+import PropTypes from "prop-types";
+const CourseListCard = ({
+  cardImage,
+  courseName,
+  coursePrice,
+  courseCategory,
+  courseInstructor,
+  courseDate,
+}) => {
   return (
-    <div>
-      <img src={CourseListImage1} />
+    <div className="w-1/3 shadow pb-4">
+      <img className="rounded-t-lg" src={cardImage} />
+      <div className="p-4 h-auto flex justify-between relative ">
+        <div className="avatar placeholder absolute -top-8">
+          <div className="bg-primary text-neutral-content rounded-full w-12">
+            <span>MX</span>
+          </div>
+        </div>
+        <div className="w-full cursor-pointer">
+          <BsThreeDotsVertical className="float-right" />
+        </div>
+      </div>
+      <div className="flex w-full justify-between mt-2 px-2">
+        <p className="font-thin underline">{courseName}</p>
+        <p className=" font-bold text-2xl  text-[#3B64DC]">{coursePrice}</p>
+      </div>
+      <div className="flex w-full justify-between mt-2 px-2">
+        <div className="flex flex-col mt-4">
+          <span className="font-thin">Category:</span>
+          <p className="font-medium">{courseCategory}</p>
+        </div>
+        <div className="flex flex-col mt-4">
+          <span className="font-thin">Instructor:</span>
+          <p className="font-medium">{courseInstructor}</p>
+        </div>
+      </div>
+
+      <div className="flex w-full gap-4 mt-8 px-2 justify-between">
+        {/* not required for now  */}
+        {/* <div className="flex gap-5">
+          <div className="flex  justify-between items-center gap-4">
+            <BsEye />
+            <span>{courseTotalViews}</span>
+          </div>
+          <div className="flex gap-4 justify-between items-center">
+            <BsChatLeftDots />
+            <span>3</span>
+          </div>
+        </div> */}
+        <div>
+          <span>{courseDate}</span>
+        </div>
+      </div>
     </div>
   );
 };
-
+CourseListCard.propTypes = {
+  cardImage: PropTypes.string.isRequired,
+  courseName: PropTypes.string.isRequired,
+  coursePrice: PropTypes.string.isRequired,
+  courseCategory: PropTypes.string.isRequired,
+  courseInstructor: PropTypes.string.isRequired,
+  courseTotalViews: PropTypes.number.isRequired,
+  courseDate: PropTypes.string.isRequired,
+};
 export default CourseListCard;
--- client/src/pages/user/UserCourseList/UserCourseList.jsx
+++ client/src/pages/user/UserCourseList/UserCourseList.jsx
@@ -1,4 +1,13 @@
 import UserProfileBanner from "../../../assets/userProfileBanner.png";
+import ImageOne from "../../../assets/courseList/ImageOne.png";
+import ImageTwo from "../../../assets/courseList/ImageTwo.png";
+import ImageThree from "../../../assets/courseList/ImageThree.png";
+import ImageFour from "../../../assets/courseList/ImageFour.png";
+import ImageFive from "../../../assets/courseList/ImageFive.png";
+import ImageSix from "../../../assets/courseList/ImageSix.png";
+import ImageSeven from "../../../assets/courseList/ImageSeven.png";
+import ImageEight from "../../../assets/courseList/ImageEight.png";
+import ImageNine from "../../../assets/courseList/ImageNine.png";
 import { Header, CourseListCard } from "../../../components";
 const UserCourseList = () => {
   return (
@@ -11,8 +20,88 @@ const UserCourseList = () => {
       <div className="mt-8">
         <h4 className="font-semibold">My Purchases</h4>
         <div className="mt-4">
-          <div className="w-full flex flex-wrap bg-teal-400">
-            <CourseListCard />
+          <div className="w-full flex  flex-wrap ">
+            <CourseListCard
+              cardImage={ImageOne}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Numerology Course"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="1231"
+            />
+            <CourseListCard
+              cardImage={ImageTwo}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageThree}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageFour}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageFive}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageSix}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageSeven}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageEight}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
+            <CourseListCard
+              cardImage={ImageNine}
+              courseName="Astro Shiromani"
+              coursePrice="$59,000"
+              courseCategory="Astrology Basic"
+              courseInstructor="Mr. Alok Khandelwal"
+              courseDate="27 May 2023"
+              courseTotalViews="3211"
+            />
           </div>
         </div>
       </div>
